0936339232c049e373a195f93665e8373e469128
Udit Arora <uditara09@gmail.com>
2016-05-28 20:50:00 +0530

    Added speed calculations and cylindrical trajectory display.

--- object-detector/3d.py
+++ object-detector/3d.py
@@ -7,6 +7,9 @@ xlist = []
 ylist = []
 zlist = []
 bouncing_pt = []
+frames_list = []
+
+FPS = 120.0
 
 START_RADIUS = 0.0
 END_RADIUS = 0.0
@@ -58,6 +61,7 @@ with open('coordinates_wideright.txt') as coord_file:
         # print x
         xlist.append(x)
         ylist.append(y)
+        frames_list.append(float(frame_no))
         # if i == 0:
         #     START_RADIUS = r_new
         # END_RADIUS = r_new
@@ -67,8 +71,8 @@ for i,radius in enumerate(rlist):
     z = (START_RADIUS-radius)/(START_RADIUS-END_RADIUS)
     zlist.append(z*SCALE[2])
 
-coord_file.close()
 # textFile = open("3d_debug.txt", "w")
+coord_file.close()
 
 # Draw environment
 scene1 = display(title="Automated Cricket Umpiring - HawkEye", width=1280, height=720, range=10, background=(0.2,0.2,0.2), center=(0,30,30))
@@ -78,18 +82,18 @@ floor_outer = box(pos=(0,0,0), size=(PITCH_LENGTH*1.25,PITCH_THICKNESS,PITCH_WID
 floor_impact = box(pos=(0,0,0), size=(PITCH_LENGTH,PITCH_THICKNESS*1.3,WICKET_WIDTH), material=materials.unshaded, color=(0.63,0.57,0.93), opacity=0.8)
 
 # Draw wickets and crease lines at batting side
-batting_wicket1 = box(pos=(PITCH_LENGTH/2,WICKET_HEIGHT/2,-(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,71.1,STUMP_WIDTH), color=color.white)
-batting_wicket2 = box(pos=(PITCH_LENGTH/2,WICKET_HEIGHT/2,0), size=(5,71.1,STUMP_WIDTH), color=color.white)
-batting_wicket3 = box(pos=(PITCH_LENGTH/2,WICKET_HEIGHT/2,(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,71.1,STUMP_WIDTH), color=color.white)
+batting_wicket1 = box(pos=(PITCH_LENGTH/2,WICKET_HEIGHT/2,-(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,WICKET_HEIGHT,STUMP_WIDTH), color=color.white)
+batting_wicket2 = box(pos=(PITCH_LENGTH/2,WICKET_HEIGHT/2,0), size=(5,WICKET_HEIGHT,STUMP_WIDTH), color=color.white)
+batting_wicket3 = box(pos=(PITCH_LENGTH/2,WICKET_HEIGHT/2,(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,WICKET_HEIGHT,STUMP_WIDTH), color=color.white)
 line1 = box(pos=(PITCH_LENGTH/2,PITCH_THICKNESS/2,0), size=(LINE_WIDTH,5,WIDE_WIDTH), color=color.white)
 line2 = box(pos=(PITCH_LENGTH/2,PITCH_THICKNESS/2,132), size=(244,5,LINE_WIDTH), color=color.white)
 line3 = box(pos=(PITCH_LENGTH/2,PITCH_THICKNESS/2,-132), size=(244,5,LINE_WIDTH), color=color.white)
 line4 = box(pos=(PITCH_LENGTH/2-122,PITCH_THICKNESS/2,0), size=(LINE_WIDTH,5,366), color=color.white)
 
 # Draw wickets at bowling side
-bowling_wicket1 = box(pos=(-PITCH_LENGTH/2,WICKET_HEIGHT/2,-(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,71.1,STUMP_WIDTH), color=color.white)
-bowling_wicket2 = box(pos=(-PITCH_LENGTH/2,WICKET_HEIGHT/2,0), size=(5,71.1,STUMP_WIDTH), color=color.white)
-bowling_wicket3 = box(pos=(-PITCH_LENGTH/2,WICKET_HEIGHT/2,(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,71.1,STUMP_WIDTH), color=color.white)
+bowling_wicket1 = box(pos=(-PITCH_LENGTH/2,WICKET_HEIGHT/2,-(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,WICKET_HEIGHT,STUMP_WIDTH), color=color.white)
+bowling_wicket2 = box(pos=(-PITCH_LENGTH/2,WICKET_HEIGHT/2,0), size=(5,WICKET_HEIGHT,STUMP_WIDTH), color=color.white)
+bowling_wicket3 = box(pos=(-PITCH_LENGTH/2,WICKET_HEIGHT/2,(WICKET_WIDTH/2-STUMP_WIDTH/2)), size=(5,WICKET_HEIGHT,STUMP_WIDTH), color=color.white)
 line1 = box(pos=(-PITCH_LENGTH/2,PITCH_THICKNESS/2,0), size=(LINE_WIDTH,5,WIDE_WIDTH), color=color.white)
 line2 = box(pos=(-PITCH_LENGTH/2,PITCH_THICKNESS/2,132), size=(244,5,LINE_WIDTH), color=color.white)
 line3 = box(pos=(-PITCH_LENGTH/2,PITCH_THICKNESS/2,-132), size=(244,5,LINE_WIDTH), color=color.white)
@@ -112,7 +116,7 @@ coords_3d = []
 
 # y_correction = 0
 
-detected_points = len(xlist)
+num_detected_points = len(xlist)
 for i in range(len(xlist)):
     # rate(2)
 
@@ -140,38 +144,85 @@ linearReg = temp1.quadraticRegression(coords_3d)
 
 # Append predicted points to coords_3d list
 for idx in range(1,300):
-    coords_3d.append(((coords_3d[detected_points-1][0] + idx),0,0,0,0))
+    coords_3d.append(((coords_3d[num_detected_points-1][0] + idx),0,0,0,0))
 
 # y_correction = quadraticReg[y_correction - 1]
 
+# Stores the final regressed 3D coordinates
+final_coords_3d = []
+
 # Draw balls at detected points
-for idx in range(detected_points):
+for idx in range(num_detected_points):
     # rate(2)
+    final_coords_3d.append((coords_3d[idx][0],quadraticReg[idx], linearReg[idx]))
     # Draw detected ball at position
     if coords_3d[idx][0] > -400:
         balls.append(sphere(pos=(coords_3d[idx][0],quadraticReg[idx], linearReg[idx]), radius=BALL_RADIUS, color=(0.52,0.15,0.19)))
         # balls.append(sphere(pos=(coords_3d[idx][0],quadraticReg[idx], coords_3d[idx][2]), radius=6, color=color.blue))
+        # Draw cylindrical trajectory
+        if idx > 0:
+            displacement = (final_coords_3d[idx][0]-final_coords_3d[idx-1][0], final_coords_3d[idx][1]-final_coords_3d[idx-1][1], final_coords_3d[idx][2]-final_coords_3d[idx-1][2])
+            cylinder(pos=(final_coords_3d[idx-1][0], final_coords_3d[idx-1][1], final_coords_3d[idx-1][2]), axis=displacement, radius=BALL_RADIUS, color=(0.52,0.15,0.19), opacity=0.3)
+
+for idx in range(num_detected_points, len(coords_3d)):
+    final_coords_3d.append((coords_3d[idx][0],quadraticReg[idx], linearReg[idx]))
 
 # Draw balls at predicted points
-for idx in range(detected_points, len(coords_3d),20):
+STEP_SIZE = 20
+for idx in range(num_detected_points, len(coords_3d),20):
     # rate(2)
     # Draw predicted ball at position, skipping 20 positions
     if coords_3d[idx][0] > -400:
-        balls.append(sphere(pos=(coords_3d[idx][0],quadraticReg[idx], linearReg[idx]), radius=BALL_RADIUS, color=color.blue))
+        balls.append(sphere(pos=(coords_3d[idx][0],quadraticReg[idx], linearReg[idx]), radius=BALL_RADIUS, color=color.blue, opacity=0.8))
         # balls.append(sphere(pos=(coords_3d[idx][0],quadraticReg[idx], coords_3d[idx][2]), radius=6, color=color.blue))
+        # Draw cylindrical trajectory
+        if idx-STEP_SIZE > 0 and idx > num_detected_points:
+            displacement = (final_coords_3d[idx][0]-final_coords_3d[idx-STEP_SIZE][0], final_coords_3d[idx][1]-final_coords_3d[idx-STEP_SIZE][1], final_coords_3d[idx][2]-final_coords_3d[idx-STEP_SIZE][2])
+            cylinder(pos=(final_coords_3d[idx-STEP_SIZE][0], final_coords_3d[idx-STEP_SIZE][1], final_coords_3d[idx-STEP_SIZE][2]), axis=displacement, radius=BALL_RADIUS, color=color.blue, opacity=0.3)
 
-def get_nearest_ball_params(wide=False):
+# Stores the speed at given index
+speed_list = []
+# Stores the average delivery speed before bounce
+average_speed = 0.0
+
+# Calculate speed at detected points
+for idx in range(num_detected_points):
+    if idx == 0:
+        speed_list.append(0)
+    else:
+        displacement = vector(final_coords_3d[idx][0]-final_coords_3d[idx-1][0], final_coords_3d[idx][1]-final_coords_3d[idx-1][1], final_coords_3d[idx][2]-final_coords_3d[idx-1][2])
+        time = float(frames_list[idx]-frames_list[idx-1])/FPS
+        speed = (mag(displacement)/time)*3.6/100
+        speed_list.append(speed)
+        if idx <= bouncing_pt_idx:
+            average_speed += speed
+
+    # Debug printing
+    # print "Speed[{}]: {}".format(idx, speed_list[idx])
+    # if idx > 1 and abs(speed_list[idx]-speed_list[idx-1])/speed_list[idx-1] > 0.1:
+    #     print "\nAnomally detected at idx: {} and frame_no: {}".format(idx, frames_list[idx])
+    #     print "x1: {}, y1: {}, z1: {}\nx2: {}, y2: {}, z2: {}\n".format(final_coords_3d[idx-1][0], final_coords_3d[idx-1][1], final_coords_3d[idx-1][2], final_coords_3d[idx][0], final_coords_3d[idx][1], final_coords_3d[idx][2])
+    #     box(pos=(final_coords_3d[idx][0],final_coords_3d[idx][1]/2,final_coords_3d[idx][2]), size=(5,final_coords_3d[idx][1],5))
+
+average_speed = average_speed/(bouncing_pt_idx+1)
+
+print "Speed of delivery: {:.3f} km/h".format(average_speed)
+
+
+""" Umpiring decisions below """
+
+def get_nearest_ball_params(near_crease=False):
     """
         Returns the index of the ball closest to wicket, absolute difference in position
         of nearest ball, and index of the last ball just before the wicket
     """
     near_wicket_idx = 0
-    before_wicket_idx = detected_points-1
+    before_wicket_idx = num_detected_points-1
     min_diff = 1000000
 
     # The distance from which to check nearest ball
     dist = PITCH_LENGTH/2
-    if wide:
+    if near_crease:
         dist = PITCH_LENGTH/2-122
 
     for i in range(len(coords_3d)):
@@ -179,24 +230,24 @@ def get_nearest_ball_params(wide=False):
         if diff < min_diff:
             min_diff = diff
             near_wicket_idx = i
-        if dist-coords_3d[i][0] < 0 and before_wicket_idx == detected_points-1:
+        if dist-coords_3d[i][0] < 0 and before_wicket_idx == num_detected_points-1:
             before_wicket_idx = i-1
     return near_wicket_idx, min_diff, before_wicket_idx
 
-def get_nearest_ball_coords(idx, min_diff, before_wicket_idx, wide=False):
+def get_nearest_ball_coords(idx, min_diff, before_wicket_idx, near_crease=False):
     """
         Returns the y and z coordinates of the ball closest to the wicket
     """
 
     # The distance from which to check nearest ball
     dist = PITCH_LENGTH/2
-    if wide:
+    if near_crease:
         dist = PITCH_LENGTH/2-122
 
     # There is a ball position at the wicket
     if abs(min_diff) <= 1:
-        y = quadraticReg[idx]
-        z = linearReg[idx]
+        y = final_coords_3d[idx][1]
+        z = final_coords_3d[idx][2]
         return y, z
     else:
         # Find coordinates of ball at wicket using linear approximation
@@ -246,6 +297,7 @@ def check_nearest_coord(idx, min_diff, before_wicket_idx):
     """
         Checks if the ball hits the wicket
     """
+    # Get y,z coordinates of ball closeset to wicket
     y, z = get_nearest_ball_coords(idx, min_diff, before_wicket_idx)
 
     if y <= (WICKET_HEIGHT+BALL_RADIUS) and z >= -(WICKET_WIDTH/2+BALL_RADIUS) and z <= WICKET_WIDTH/2+BALL_RADIUS:
@@ -265,8 +317,9 @@ def check_wide():
     """
         Checks if the ball delivery would result in wide decision
     """
-    near_wicket_idx, min_diff, before_wicket_idx = get_nearest_ball_params(wide=True)
-    y, z = get_nearest_ball_coords(near_wicket_idx, min_diff, before_wicket_idx, wide=True)
+    # Get y,z coordinates of the ball closest to crease
+    near_wicket_idx, min_diff, before_wicket_idx = get_nearest_ball_params(near_crease=True)
+    y, z = get_nearest_ball_coords(near_wicket_idx, min_diff, before_wicket_idx, near_crease=True)
 
 
     # Wide debug displays
@@ -282,3 +335,12 @@ if check_wide():
     print "\nWIDE DECISION: WIDE"
 else:
     print "\nWIDE DECISION: NOT WIDE"
+
+def check_bouncer():
+    """
+        Checks if the ball delivery would result in bouncer
+    """
+    # Get y,z coordinates of the ball closest to crease
+    near_wicket_idx, min_diff, before_wicket_idx = get_nearest_ball_params(near_crease=True)
+    y, z = get_nearest_ball_coords(near_wicket_idx, min_diff, before_wicket_idx, near_crease=True)
+
